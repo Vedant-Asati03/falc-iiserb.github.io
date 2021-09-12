f3430f38956a8f32ede4cbd3b4b176f92c76236f
Priyanshu <priyanshu1@outlook.in>
2021-09-12 18:01:17 +0530

    Timer context correction #2

--- a/assets/js/timer.js
+++ a/assets/js/timer.js
@@ -18,7 +18,7 @@ var x = setInterval(function() {
     
   // Output the result in an element with id="demo"
   document.getElementById("timer").innerHTML = days + ":" + hours + ":"
-  + minutes + ":" + seconds + "<pre style=\"font-family: bdfUM; font-size:20px; margin-top: -5px;\">days  hrs mins secs</pre>";
+  + minutes + ":" + seconds + "<pre style=\"font-family: bdfUM; font-size:20px; margin-top: -5px;\">days  hrs  mins  secs</pre>";
     
   // If the count down is over, write some text 
   if (distance < 0) {
